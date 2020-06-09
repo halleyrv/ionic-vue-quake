51122ef5dba4044bda774f932c6e9f0f2df8a2b0
=Halley Rios <francrv@hotmail.com>
2020-06-09 17:15:13 -0500

    chore(:vue-google-maps) if applied this commit, will install google maps and configure this in your main.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,25 @@ import router from './router'
 import Ionic from "@ionic/vue"
 import "@ionic/core/css/core.css"
 import "@ionic/core/css/ionic.bundle.css"
+import * as VueGoogleMaps from 'vue2-google-maps'
+
 
 Vue.config.productionTip = false
 Vue.use(Ionic)
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'PUT HERE YOUR KEY GOOGLE',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+  installComponents: true
+})
 
 new Vue({
   router,
